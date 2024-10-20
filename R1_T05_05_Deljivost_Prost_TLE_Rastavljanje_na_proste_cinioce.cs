f35ae5ef1d1d21b58de4483e49f4f3dcef7c835f
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2024-10-20 12:28:26 +0200

    Update R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce.cs

--- R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce.cs
+++ R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce.cs
@@ -2,6 +2,8 @@
 // https://petlja.org/sr-Latn-RS/biblioteka/r/problemi/Zbirka-stara/rastavljanje_na_proste_cinioce
 // https://arena.petlja.org/sr-Latn-RS/competition/r1-t05-deljivost-prosti-tle-001-2024#tab_132321
 // https://petlja.org/sr-Latn-RS/kurs/14606/23/2756
+// https://github.com/draganilicnis/R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce/blob/main/R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce.cs
+// https://onlinegdb.com/Xn72wK0Vb
 
 using System;
 
@@ -14,6 +16,15 @@ class R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce
         int d = 2;  // Prvi potencijalni prost cinilac je 2
         while (d * d <= n)
         {
+
+            while (n % d == 0)
+            {
+                Console.Write(d + " ");
+                n = n / d;
+            }
+            d++;       //   prelazimo na sledeceg kandidata
+            
+            /*
             if (n % d == 0)
             {
                 Console.Write(d + " ");
@@ -23,6 +34,7 @@ class R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce
             {
                 d++;
             }
+            */
         }
         if (n > 1) Console.Write(n);
     }
