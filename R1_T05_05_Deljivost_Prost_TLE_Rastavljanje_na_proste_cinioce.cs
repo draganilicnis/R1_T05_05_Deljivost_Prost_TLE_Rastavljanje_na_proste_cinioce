76487af4aefb36bdc6092758fcc4806c057c46a0
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2025-10-26 17:45:12 +0100

    Update R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce.cs

--- R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce.cs
+++ R1_T05_05_Deljivost_Prost_TLE_Rastavljanje_na_proste_cinioce.cs
@@ -6,24 +6,63 @@ class R1_T05_05_Faktorizacija_Rastavljanje_na_proste_cinioce
 {
     static void Main()
     {
-        ulong n = ulong.Parse(Console.ReadLine()); // Faktorizacija_Prosti_cinioci_Write_000(n);
+        long n = long.Parse(Console.ReadLine()); // Faktorizacija_Prosti_cinioci_Write_000(n);
         // ulong n = 900;   // n = 600;
-        // Stopwatch t = new Stopwatch();
+        //Stopwatch t = new Stopwatch();
         // n = 1000 * 1000 * 1000 + 7;          // n = 10^9 + 7
-        // n = 1111111111111111111;                // n > 10^19
+        //n = 1111111111111111111;                // n > 10^19
         // t.Start(); Faktorizacija_Prosti_cinioci_Write_000(n); t.Stop(); Console.WriteLine(t.Elapsed); t.Reset();    // oko 8 sec
         // t.Start(); Faktorizacija_Prosti_cinioci_Write_001(n); t.Stop(); Console.WriteLine(t.Elapsed); t.Reset();    // oko 9 sec
-        // t.Start();
-        // Console.WriteLine("\nProsti cinioci: Lista");
-        // List<ulong> Cinioci_prosti_Lista = Faktorizacija_Prosti_cinioci_Lista(n);
-        // for (int d = 0; d < Cinioci_prosti_Lista.Count; d++) Console.Write(Cinioci_prosti_Lista[d] + " ");              // oko 7 sec
-        // t.Stop(); Console.WriteLine(t.Elapsed); t.Reset();
-
+        
+        //t.Start();
+        //Console.WriteLine("Cinioci PROSTI: Lista:");
+        List<long> Cinioci_prosti_Lista = Faktorizacija_Prosti_cinioci_Lista(n);
+        for (int d = 0; d < Cinioci_prosti_Lista.Count; d++) Console.Write(Cinioci_prosti_Lista[d] + " ");              // oko 7 sec
+        //Console.WriteLine();
+        //Console.WriteLine(Cinioci_prosti_Lista.Count);
+        //Console.WriteLine(Cinioci_prosti_Lista.Sum());
+        //t.Stop(); Console.WriteLine(t.Elapsed); t.Reset();
+        
+        /*
+        t.Start();
+        Console.WriteLine("Cinioci SVI (delioci): Lista:");
+        List<long> Cinioci_svi_Lista = Faktorizacija_Cionioci_svi_Lista(n);
+        Cinioci_svi_Lista.Add(1); if (n > 1) Cinioci_svi_Lista.Add(n);         // Dodajemo kao delioce i 1 i N.
+        for (int d = 0; d < Cinioci_svi_Lista.Count; d++) Console.Write(Cinioci_svi_Lista[d] + " ");                    // oko 7 sec
+        Console.WriteLine();
+        Console.WriteLine(Cinioci_svi_Lista.Count); // Console.WriteLine(Cinioci_svi_Lista.Count + 2);
+        Console.WriteLine(Cinioci_svi_Lista.Sum()); // Console.WriteLine(Cinioci_svi_Lista.Sum() + 1 + n);
+        t.Stop(); Console.WriteLine(t.Elapsed); t.Reset();
+        */
         // t.Start();
         // Console.WriteLine("\nProsti cinioci: Recnik");
-        Dictionary<ulong, int> Cinioci_prosti_Recnik = Faktorizacija_Prosti_cinioci_Recnik(n);                        // oko 7 sec
-        foreach (var x in Cinioci_prosti_Recnik) for (int i = 0; i < x.Value; i++) Console.Write(x.Key + " ");              
+        // Dictionary<ulong, int> Cinioci_prosti_Recnik = Faktorizacija_Prosti_cinioci_Recnik(n);                        // oko 7 sec
+        // foreach (var x in Cinioci_prosti_Recnik) for (int i = 0; i < x.Value; i++) Console.Write(x.Key + " ");              
         // t.Stop(); Console.WriteLine(t.Elapsed); t.Reset();
+    }
+    static List<long> Faktorizacija_Prosti_cinioci_Lista(long n)
+    {
+        List<long> a = new List<long>();
+        long d = 2;
+        while (d * d <= n)      // Sa proverom deljivosti prestajemo kada potencijalni delilac d premasi vrednost Sqrt(N).
+        {
+            if (n % d == 0) { a.Add(d); n = n / d; }
+            else d++;
+        }
+        if (n > 1) a.Add(n); // Ako je u tom trenutku (kada potencijalni delilac d premasi vrednost Sqrt(N), 
+        return a;            // tekuca vrednost N ako je veca od 1, predstavlja jedini preostali prost delilac polazne vrednosti N.   
+    }
+    static List<long> Faktorizacija_Cionioci_svi_Lista(long n)
+    {
+        List<long> a = new List<long>();
+        long d = 2;
+        while (d * d < n)      // Sa proverom deljivosti prestajemo kada potencijalni delilac d premasi vrednost Sqrt(N).
+        {
+            if (n % d == 0) { a.Add(d); a.Add(n / d); } 
+            d++;
+        }
+        if (d * d == n) a.Add(d); // Ako je u tom trenutku (kada potencijalni delilac d dostigne ili premasi vrednost Sqrt(N), 
+        return a;                 // tekuca vrednost d = Sqrt(N), onda je on delilac broja N (ali nema par).   
     }
     static Dictionary<ulong, int> Faktorizacija_Prosti_cinioci_Recnik(ulong n)
     {
@@ -39,20 +78,6 @@ class R1_T05_05_Faktorizacija_Rastavljanje_na_proste_cinioce
         if (n > 1) if (!a.ContainsKey(n)) a[n] = 0; a[n]++; // Ako je u tom trenutku (kada potencijalni delilac d premasi vrednost Sqrt(N), 
         return a;            // tekuca vrednost N ako je veca od 1, predstavlja jedini preostali prost delilac polazne vrednosti N.   
     }
-    static List<ulong> Faktorizacija_Prosti_cinioci_Lista(ulong n)
-    {
-        List<ulong> a = new List<ulong>();
-        ulong d = 2;
-        while (d * d <= n)      // Sa proverom deljivosti prestajemo kada potencijalni delilac d premasi vrednost Sqrt(N).
-            if (n % d == 0)
-            {
-                a.Add(d);
-                n = n / d;
-            }
-            else d++;
-        if (n > 1) a.Add(n); // Ako je u tom trenutku (kada potencijalni delilac d premasi vrednost Sqrt(N), 
-        return a;            // tekuca vrednost N ako je veca od 1, predstavlja jedini preostali prost delilac polazne vrednosti N.   
-    }                                    
     static void Faktorizacija_Prosti_cinioci_Write_000(ulong n)
     {
         ulong d = 2;
@@ -92,3 +117,8 @@ class R1_T05_05_Faktorizacija_Rastavljanje_na_proste_cinioce
 // https://arena.petlja.org/sr-Latn-RS/competition/r1-03-petlje-04-deljivost#tab_91514
 // https://arena.petlja.org/sr-Latn-RS/competition/r2-t01-slozenost-t02-02-zamena-iteracije-dom#tab_132321
 // https://arena.petlja.org/sr-Latn-RS/competition/r1-t05-05-prost-broj#tab_132321
+//
+// Ako se traze svi delioci broja N (svi parovi delilaca)
+// https://petlja.org/sr-Latn-RS/biblioteka/r/Zbirka3/broj_i_zbir_delilaca
+// https://www.petlja.org/sr-Latn-RS/biblioteka/r/Zbirka3/broj_i_zbir_delilaca
+// https://arena.petlja.org/sr-Latn-RS/competition/r1-t05-05-prost-broj#tab_134539
